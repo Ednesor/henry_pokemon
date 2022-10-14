794449758114108edc20b176edb581873eadf74a
Ednesor <edgar.funes2001@gmail.com>
2022-10-14 19:10:46 -0300

    router y navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Pokemon</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <h1>Pagina de inicio</h1>
+        </Route>
+        <Route path="/home">
+          <h1>Home con las cards y los filtros</h1>
+        </Route>
+        <Route path="/create">
+          <h1>Crear pokemon</h1>
+        </Route>
+        <Route path="/details/:id">
+          <h1>detalles</h1>
+        </Route>
+        <Route path="*">
+          <h1>Error, la pagina no existe :(</h1>
+        </Route>
+        {/*//! no poner ruta search */}
+      </Switch>
+    </Router>
   );
 }
 
---
+++ client/src/components/Navbar/Nabvar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import style from "./Navbar.module.css";
+
+export default function Navbar(){
+    return(
+        <div>
+            <ul>
+                <li>
+                    Home
+                </li>
+                <li>
+                    Create
+                </li>
+            </ul>
+        </div>
+    )
+}
