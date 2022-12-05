9c30c61ebc484cbbd947bfa59f7f3d6e22131266
ednesor <edgar.funes2001@gmail.com>
2022-12-05 20:33:25 -0300

    deploy config

---
+++ api/config.js
@@ -0,0 +1,7 @@
+const PORT = process.env.PORT || 3001;
+const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_USER = process.env.DB_USER || "root";
+const DB_PASSWORD = process.env.DB_PASSWORD || "password";
+const DB_NAME = process.env.DB_NAME || "userdb";
+
+module.exports = { PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME }
